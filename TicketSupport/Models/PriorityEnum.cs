948440edd0568b1174d665fa0f4463c9c5165645
VirginVaga <povar359@gmail.com>
2017-09-10 18:46:32 +0000

    Refactor

--- TicketSupport/Models/PriorityEnum.cs
+++ TicketSupport/Models/PriorityEnum.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicketSupport.Models
+﻿namespace TicketSupport.Models
 {
     public enum Priority
     {
